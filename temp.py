916f016eb340c700ba05cb2a9e713638002f7199
Lopezveloz <31290154+Lopezveloz@users.noreply.github.com>
2017-08-30 12:59:06 -0500

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,7 +1,7 @@
 #Python usa librerias como matplotlib con objetos predeterminados como pyplot que tienen propiedades que permiten enviar las instrucciones a la maquina de una forma resumida. Matplotlib es una libreria para crear graficas en 2 dimensiones. 
 import matplotlib.pyplot as plt
 #Construimos el objeto plt introduciendo valores que se grafican contra el numero de entrada. Por ejemplo si queremos graficar el crecimiento de un material por dia en centimetros. 
-plt.plot([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+plt.plot([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 #Con esta isntruccion colocamos un titulo al eje Y de la grafica. 
 plt.ylabel('Numero de hermanos y primos')
 #Con esta isntruccion guardamos la imagen con el formato que queramos.
